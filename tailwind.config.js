c18155f3e782a58884653248f580022caca5ce5b
CalebeSG <calebeguimaraes10@gmail.com>
2022-06-08 14:32:36 -0300

    added custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 module.exports = {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+  content: ['./src/**/*.{html,js,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          400: 'hsl(0, 0%, 59%)',
+          800: 'hsl(0, 0%, 17%)',
+        },
+      },
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
